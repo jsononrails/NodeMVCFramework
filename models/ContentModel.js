0d9607e2d15ee885ab49f906519ba00fc0f0b1e6
Jason McBride <jsononrails@gmail.com>
2013-11-16 19:35:45 -0500

    stubbed content model

---
+++ models/ContentModel.js
@@ -0,0 +1,26 @@
+// /models/ContentModel.js
+
+var Model = require("./Base"),
+	model = new Model();
+	
+var ContentModel = model.extend{
+	
+	insert: function(data, callback) {
+
+	},
+	
+	update: function(data, callback) {
+		
+	},
+	
+	getlist: function(data, callback) {
+		
+	},
+	
+	remove: function(ID, callback) {
+		
+	}
+	
+});
+
+module.exports = ContentModel;
