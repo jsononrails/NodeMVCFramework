49e7e0f8a85013c66d16e5eacb59ce7a51d9350a
Jason McBride <jsononrails@gmail.com>
2014-05-09 14:54:18 -0400

    updated create user table script

--- data/scripts/pg_users_table.sql
+++ data/scripts/pg_users_table.sql
@@ -6,11 +6,11 @@ CREATE TABLE users
 (
   user_id character varying(25) NOT NULL,
   display_name character varying(30) NOT NULL,
+  gender character varying(6) NOT NULL,
+  profile_url character varying(50) NOT NULL,
   date_created date not null default CURRENT_DATE,
   CONSTRAINT users_pkey PRIMARY KEY (user_id)
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE users
-  OWNER TO squireshand;
